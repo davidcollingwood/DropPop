7548df7ee0ed368bdcfa343c144489e048e6cf33
David Collingwood <david.collingwood@crudigital.com.au>
2014-09-27 15:01:12 +1000

    Add friend page and controller. Give friends page proper content. Add “models” module. Add user and friend models. Upgrade to ionic beta 13.

--- app/scripts/controllers/FriendsCtrl.js
+++ app/scripts/controllers/FriendsCtrl.js
@@ -1,6 +1,20 @@
 angular.module('droppop.controllers')
 
-    .controller('FriendsCtrl', function($scope) {
+    .controller('FriendsCtrl', function($scope, $ionicNavBarDelegate, user) {
+        
+        $scope.is_searching = false;
+        $scope.friends_filter = '';
+        $scope.friends = user.friends;
+        
+        $scope.search = function() {
+            $ionicNavBarDelegate.showBar(false);
+            $scope.is_searching = true;
+        };
+        
+        $scope.cancelSearch = function() {
+            $ionicNavBarDelegate.showBar(true);
+            $scope.is_searching = false;
+        };
         
     })
 
