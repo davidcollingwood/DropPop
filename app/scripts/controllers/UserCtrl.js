fd01b09b0da930df87d7d78f9b69985c5e28f955
David Collingwood <david.collingwood@crudigital.com.au>
2014-10-19 05:03:34 +1000

    Minor bug fix to article links on user page.

--- app/scripts/controllers/UserCtrl.js
+++ app/scripts/controllers/UserCtrl.js
@@ -1,12 +1,16 @@
 angular.module('droppop.controllers')
 
-    .controller('UserCtrl', function($scope, user) {
+    .controller('UserCtrl', function($scope, user, Article) {
         
         $scope.is_me = true;
         $scope.profile = user.getProfile();
         
         $scope.getFriendClass = function() {
             return '';
+        };
+        
+        $scope.getArticleId = function(article) {
+            return Article.getArticleId(article);
         };
         
     })
