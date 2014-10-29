ac01c729360157624c303b5f4c72c680669bdfe8
David Collingwood <david.collingwood@crudigital.com.au>
2014-10-29 14:09:06 +1000

    Update AR world to use api data

--- app/assets/world/world.js
+++ app/assets/world/world.js
@@ -1,29 +1,15 @@
 // implementation of AR-Experience (aka "World")
 var World = {
-    // ranges for random values
-    range: {
-        latitude: 0.0001,
-        longitude: 0.0001,
-        altitude: 1
-    },
-    
-    // user's current position
-    position: {
-        latitude: 0,
-        longitude: 0,
-        altitude: 0,
-        accuracy: 0
-    },
     
 	// true once data was fetched
 	initiallyLoadedData: false,
 
 	// called to inject new POI data
-	loadPoisFromJsonData: function loadPoisFromJsonDataFn(poiData) {
-		var markerLocation = new AR.GeoLocation(poiData.latitude, poiData.longitude, poiData.altitude);
+	loadPoisFromJsonData: function loadPoisFromJsonDataFn(bubble) {
+		var markerLocation = new AR.GeoLocation(bubble.location.lat, bubble.location.lng, 100.0);
 		var markerHtmlDrawable = new AR.HtmlDrawable({ uri: 'bubble.html' }, 5, {
     		onClick: function() {
-    		    alert('tap tap tap');
+    		    alert('Article: ' + bubble.article.title + '\nDropped by: ' + bubble.user.full_name);
         		window.location = 'architectsdk://close';
     		}
 		});
@@ -35,40 +21,40 @@ var World = {
 			}
 		});
 	},
-	
-	createPois: function(count) {
-    	for (var i = 0; i < count; i++) {
-    	    World.createPoi();
-    	}
-	},
-	
-	createPoi: function() {
-	    var lat = World.position.latitude - (World.range.latitude / 2) + Math.random() * World.range.latitude;
-	    var lon = World.position.longitude - (World.range.longitude / 2) + Math.random() * World.range.longitude;
-	    var alt = World.position.altitude - (World.range.altitude / 2) + Math.random() * World.range.altitude;
-	    
-    	World.loadPoisFromJsonData({
-        	latitude: lat,
-        	longitude: lon,
-        	altitude: alt
-    	});
-	},
 
 	// location updates, fired every time you call architectView.setLocation() in native environment
 	locationChanged: function locationChangedFn(lat, lon, alt, acc) {
-	    World.position.latitude = lat;
-        World.position.longitude = lon;
-        World.position.altitude = alt;
-        World.position.accuracy = acc;
-        
-		/*
+	    /*
 			The custom function World.onLocationChanged checks with the flag World.initiallyLoadedData if the function was already called. With the first call of World.onLocationChanged an object that contains geo information will be created which will be later used to create a marker using the World.loadPoisFromJsonData function.
 		*/
 		if (!World.initiallyLoadedData) {
-		    World.createPois(20);
+		    World.loadData();
 			World.initiallyLoadedData = true;
 		}
 	},
+	
+	loadData: function() {
+    	var request = new XMLHttpRequest();
+    	
+    	request.onreadystatechange = function() {
+        	if (request.readyState != 4)
+        	    return;
+            if (request.status != 200) {
+                alert('The server is not responding...please try again.');
+                return;
+            }
+            
+            var data = JSON.parse( request.responseText );
+            var bubbles = data.data.bubbles;
+            
+            for (var i = 0; i < bubbles.length; i++) {
+                World.loadPoisFromJsonData(bubbles[i]);
+            }
+    	};
+    	
+    	request.open('GET', 'http://drop-pop-api.gopagoda.com/api/bubbles', true);
+    	request.send(null);
+	}
 };
 
 /* 
